4fa94744d6895f868c59d906b20e5a95189aa2e3
Tanel Pauls <paulstanel@protonmail.com>
2024-10-02 21:51:39 +0300

    Draws circles on click

--- CanvasProject1.js
+++ CanvasProject1.js
@@ -6,9 +6,20 @@ canvas.height=window.innerHeight;
 window.addEventListener("resize",function(){
     canvas.width=window.innerWidth;
     canvas.height=window.innerHeight;
-    canvasContext.fillStyle="white";
-    canvasContext.fillRect(10,10,50,50);
-})
+});
 
-canvasContext.fillStyle="white";
-canvasContext.fillRect(10,10,50,50);
+const mouse={
+    x:null,
+    y:null,
+}
+canvas.addEventListener('click',function(event){
+    mouse.x=event.x;
+    mouse.y=event.y;
+    drawCircle();
+})
+function drawCircle(){
+    canvasContext.fillStyle='blue';
+    canvasContext.beginPath();
+    canvasContext.arc(mouse.x, mouse.y, 50, 0, Math.PI * 2);
+    canvasContext.fill();
+}
