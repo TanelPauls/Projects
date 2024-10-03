7d97e477ac5c9dc704e15ea12a9a75e7df5d124f
Tanel Pauls <paulstanel@protonmail.com>
2024-10-04 00:58:56 +0300

    CanvasProject3: Draws a canvas over a picture

--- CanvasProject3ImageToPixels.js
+++ CanvasProject3ImageToPixels.js
@@ -1,13 +1,16 @@
 const canvas=document.getElementById("CanvasProject3ImageToPixels")
 const ctx=canvas.getContext("2d");
-canvas.width=window.innerWidth;
-canvas.height=window.innerHeight;
+const img = document.getElementById('imageToOverlay');
+canvas.width=img.clientWidth;
+canvas.height=img.clientHeight;
 
 window.addEventListener("resize",function(){
-    canvas.width=window.innerWidth;
-    canvas.height=window.innerHeight;
-    ctx.fillStyle="red";
-    ctx.canvasContext.fillRect(55,55,80,80);
+    canvas.width=img.clientWidth;
+    canvas.height=img.clientHeight;
+    ctx.fillStyle="black";
+    ctx.fillRect(10,10,50,50);
 })
+
+//draw a canvas over a picture
 ctx.fillStyle="black";
 ctx.fillRect(10,10,50,50);
