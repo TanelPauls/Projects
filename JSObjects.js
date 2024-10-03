69ff7274fbad08c0997ecc708c5608e8a1662ab0
Tanel Pauls <paulstanel@protonmail.com>
2024-10-03 10:38:09 +0300

    some object with functions

--- JSObjects.js
+++ JSObjects.js
@@ -2,8 +2,27 @@ const person = new Object();
 person.firstName = "John";
 person.lastName = "Doe";
 person.age = 33;
-person.eyeColor = "blue"; 
+person.eyeColor = "blue";
+person.fullName = function() { // Define fullName as a function
+  return this.firstName + " " + this.lastName;
+};
 
-// Diplay Object Content
+const person2 = {
+    firstName: "Johnny",
+    lastName: "Black",
+    id: 5566,
+    age: 34,
+    eyeColor: "black",
+    fullName() {
+      return this.firstName + " " + this.lastName;
+    }
+};
+
+
 document.getElementById("demo").innerHTML =
-person.firstName + " is " + person.age + " years old.";
+person.firstName + " is " + person.age + " years old. "+
+"Their eye color is " +person["eyeColor"]+". "+person.fullName();
+
+document.getElementById("demo2").innerHTML =
+person2.firstName + " is " + person2.age + " years old. "+
+"Their eye color is " +person2["eyeColor"]+". "+person2.fullName();
