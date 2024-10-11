c67bc8e8487d80a736638885cabd450b4db64469
Tanel Pauls <paulstanel@protonmail.com>
2024-10-11 15:39:32 +0300

    Hotell:poolik

---
+++ Hotelileht/hotell.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var yearSpan = document.getElementById('current-year');
+    var currentYear = new Date().getFullYear();
+    yearSpan.textContent = currentYear;
+});
