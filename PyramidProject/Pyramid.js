d0d185334d2bef33f38e4c2079aceb6a18b92361
Tanel Pauls <paulstanel@protonmail.com>
2024-10-19 20:18:30 +0300

    Pyramid: draws rudimentary pyramid

--- PyramidProject/Pyramid.js
+++ PyramidProject/Pyramid.js
@@ -11,6 +11,29 @@ class CreateUpdateTable{
         canvas.height=500;
         this.canvasWidth=canvas.width;
         this.canvasHeight=canvas.height;
+        let pyramidRows = parseInt(document.getElementById("pyrRows").innerText);
+        for(let y=0;y<=pyramidRows+4;y++){
+            if(y!=0 && y!=1 && y!=pyramidRows+4 && y!=pyramidRows+3){
+                ctx.beginPath();
+                ctx.moveTo(0, (this.canvasHeight*y)/(pyramidRows+4));
+                ctx.lineTo(this.canvasWidth, (this.canvasHeight*y)/(pyramidRows+4));
+                ctx.strokeStyle = 'black';
+                ctx.stroke();
+            }
+            
+        }
+        ctx.beginPath();
+        ctx.moveTo(this.canvasWidth/2, (this.canvasHeight*2)/(pyramidRows+4));
+        ctx.lineTo((this.canvasHeight*2)/(pyramidRows+4), (this.canvasHeight*(pyramidRows+2))/(pyramidRows+4));
+        ctx.strokeStyle = 'black';
+        ctx.stroke();
+        
+        ctx.beginPath();
+        ctx.moveTo(this.canvasWidth/2, (this.canvasHeight*2)/(pyramidRows+4));
+        ctx.lineTo((this.canvasHeight*(pyramidRows+2))/(pyramidRows+4), (this.canvasHeight*(pyramidRows+2))/(pyramidRows+4));
+        ctx.strokeStyle = 'black';
+        ctx.stroke();
+
     }
     drawBoxes(){}
 }
