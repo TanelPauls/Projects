af1c12d340d24c3c5f3d7875af17e3285cbb7894
Tanel Pauls <paulstanel@protonmail.com>
2024-11-10 00:16:03 +0200

    LeetCode: added chat solution

--- Leetcode/0073SetMatrixZeroes.js
+++ Leetcode/0073SetMatrixZeroes.js
@@ -68,5 +68,71 @@ var setZeroes = function(matrix) {
     return matrix
 };
 
+/* var setZeroes = function(matrix) {
+    let firstRowZeroes = false;
+    let firstColumnZeroes = false;
+    let rows = matrix.length;
+    let cols = matrix[0].length;
+
+    // Check if first row has any zeroes
+    for (let j = 0; j < cols; j++) {
+        if (matrix[0][j] === 0) {
+            firstRowZeroes = true;
+            break;
+        }
+    }
+
+    // Check if first column has any zeroes
+    for (let i = 0; i < rows; i++) {
+        if (matrix[i][0] === 0) {
+            firstColumnZeroes = true;
+            break;
+        }
+    }
+
+    // Use the first row and column as flags
+    for (let i = 1; i < rows; i++) {
+        for (let j = 1; j < cols; j++) {
+            if (matrix[i][j] === 0) {
+                matrix[i][0] = 0;
+                matrix[0][j] = 0;
+            }
+        }
+    }
+
+    // Zero out cells based on flags in the first row and column
+    for (let i = 1; i < rows; i++) {
+        if (matrix[i][0] === 0) {
+            for (let j = 1; j < cols; j++) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+    for (let j = 1; j < cols; j++) {
+        if (matrix[0][j] === 0) {
+            for (let i = 1; i < rows; i++) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+    // Zero out the first row if needed
+    if (firstRowZeroes) {
+        for (let j = 0; j < cols; j++) {
+            matrix[0][j] = 0;
+        }
+    }
+
+    // Zero out the first column if needed
+    if (firstColumnZeroes) {
+        for (let i = 0; i < rows; i++) {
+            matrix[i][0] = 0;
+        }
+    }
+
+    return matrix;
+}; */
+
 matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
 console.log(setZeroes(matrix))
