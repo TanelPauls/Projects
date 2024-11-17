63ab7f9cfe4a5a88a2e1a8d44a2722514bde4abc
Tanel Pauls <paulstanel@protonmail.com>
2024-11-17 17:00:00 +0200

    ProductsPage: backend finished

--- ReactProjectProductPage/backend/server.js
+++ ReactProjectProductPage/backend/server.js
@@ -9,12 +9,13 @@ import productRoutes from "./routes/product.route.js";
 dotenv.config();
 
 const app=express();
+const PORT=process.env.PORT || 5000;
 
 app.use(express.json()); // allows us to accept JSON data in the req.body
 
 app.use("/api/products", productRoutes)
 
 connectDB();
-app.listen(5000, ()=>{
-    console.log("Server started at http://localhost:5000")
+app.listen(PORT, ()=>{
+    console.log("Server started at http://localhost:"+ PORT)
 });
