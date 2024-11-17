1ab76eaeacb9a852d8469bc0c6f7513ab80bcf01
Tanel Pauls <paulstanel@protonmail.com>
2024-11-17 15:35:18 +0200

    ProductsPageTut:  edititing works

--- ReactProjectProductPage/backend/server.js
+++ ReactProjectProductPage/backend/server.js
@@ -5,6 +5,7 @@ import express from 'express';
 import dotenv from "dotenv";
 import { connectDB } from './config/db.js';
 import Product from './config/models/product.model.js';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
@@ -40,7 +41,22 @@ app.post("/api/products", async (req,res)=>{
     }
 })
 
-//app.put("/api/products/:id", async (req,res)=>{
+app.put("/api/products/:id", async (req,res)=>{
+    const {id} =req.params;
+    
+    const product=req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({success: false, message:"Invalid Product Id"});
+    }
+
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(id, product,{new:true});
+        res.status(200).json({success: true, data: updatedProduct})
+    } catch (error) {
+        res.status(500).json({success:false, message: "Server error"});
+    }
+})
 
 app.delete("/api/products/:id", async (req,res)=>{
     const {id} = req.params
