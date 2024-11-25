2aeb24edad369fb72840271bf17b4d4ca6b2ab51
Tanel Pauls <paulstanel@protonmail.com>
2024-11-25 07:46:27 +0200

    ReactJobsProject: can delete posts now.

--- React/JobsProject/src/pages/JobPage.jsx
+++ React/JobsProject/src/pages/JobPage.jsx
@@ -4,6 +4,7 @@ import { useParams, useLoaderData, useNavigate } from 'react-router-dom';
 //import Spinner from '../components/Spinner'
 import { FaArrowLeft, FaMapMarker } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 const JobPage = ({deleteJob}) => {
     const navigate=useNavigate();
@@ -16,6 +17,7 @@ const JobPage = ({deleteJob}) => {
         if(!confirm) return;
 
         deleteJob(jobId);
+        toast.success('Job deleted successfully');
         navigate('/jobs')
     }
     //const[job, setJob] = useState(null);
