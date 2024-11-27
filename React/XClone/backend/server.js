f6cf6408187cf5297036415bbd90e9e9de2864e7
Tanel Pauls <paulstanel@protonmail.com>
2024-11-27 23:51:04 +0200

    React XClone: added GetMe

--- React/XClone/backend/server.js
+++ React/XClone/backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotevn from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongodb.js";
+import cookieParser from "cookie-parser";
 
 dotevn.config();
 const app = express();
@@ -9,6 +10,8 @@ const PORT=process.env.PORT || 5000;
 
 app.use(express.json()); // to parse req.body
 app.use(express.urlencoded({extended: true})); // to parse formdata(urlencoded)
+
+app.use(cookieParser())
 
 app.use("/api/auth", authRoutes);
 
