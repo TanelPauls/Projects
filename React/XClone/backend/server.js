bdc4083c5ea7d3f9b331660a3fc35c22382119a1
Tanel Pauls <paulstanel@protonmail.com>
2024-11-27 22:12:37 +0200

    React XClone: auth goes to db.

---
+++ React/XClone/backend/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotevn from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+import connectMongoDB from "./db/connectMongodb.js";
+
+dotevn.config();
+const app = express();
+const PORT=process.env.PORT || 5000;
+
+app.use(express.json()); // to parse req.body
+app.use(express.urlencoded({extended: true})); // to parse formdata(urlencoded)
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`)
+    connectMongoDB();
+})
