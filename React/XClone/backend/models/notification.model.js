ca1fbc234a46ad6ff33dc150f56457a2fbc61b3a
Tanel Pauls <paulstanel@protonmail.com>
2024-12-01 20:37:40 +0200

    React XClone getSuggestedUsers

--- React/XClone/backend/models/notification.model.js
+++ React/XClone/backend/models/notification.model.js
@@ -1,27 +1,30 @@
 import mongoose from "mongoose";
 
-const notificationSchema = new mongoose.Schema({
-    from:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        require: true
+const notificationSchema = new mongoose.Schema(
+  {
+    from: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      require: true,
     },
-    to:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        require: true
+    to: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      require: true,
     },
-    type:{
-        type: String,
-        require: true,
-        enum: ['follow', 'like']
+    type: {
+      type: String,
+      require: true,
+      enum: ["follow", "like"],
     },
-    read:{
-        type: Boolean,
-        default: false
-    }
-},{timestamps: true});
+    read: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
 
-const Notification = mongoose.model('Notification', notificationSchema);
+const Notification = mongoose.model("Notification", notificationSchema);
 
 export default Notification;
--- React/XClone/backend/server.js
+++ React/XClone/backend/server.js
@@ -5,22 +5,21 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
-import connectMongoDB from "./db/connectMongodb.js";
-
+import connectMongoDB from "./db/connectMongoDB.js";
 
 dotevn.config();
 const app = express();
-const PORT=process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // to parse req.body
-app.use(express.urlencoded({extended: true})); // to parse formdata(urlencoded)
+app.use(express.urlencoded({ extended: true })); // to parse formdata(urlencoded)
 
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`)
-    connectMongoDB();
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectMongoDB();
+});
