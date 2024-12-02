3cdfff05b4cdd0aa3f7cc82ae082f50655c9d347
Tanel Pauls <paulstanel@protonmail.com>
2024-12-02 22:25:38 +0200

    React XClone: Like/Unline/Comment

--- React/XClone/backend/controllers/post.controller.js
+++ React/XClone/backend/controllers/post.controller.js
@@ -1,3 +1,4 @@
+import Notification from "../models/notification.model.js";
 import Post from "../models/post.model.js";
 import User from "../models/user.model.js";
 import { v2 as cloudinary } from "cloudinary";
@@ -51,5 +52,62 @@ export const deletePost = async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: "Internal server error" });
     console.log("Error in deletePost controller: ", error);
+  }
+};
+export const commentOnPost = async (req, res) => {
+  try {
+    const { text } = req.body;
+    const postId = req.params.id;
+    const userId = req.user._id;
+
+    if (!text) {
+      return res.status(400).json({ error: "Text field is required" });
+    }
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res.status(400).json({ error: "Post not found" });
+    }
+    const comment = {
+      user: userId,
+      text,
+    };
+    post.comments.push(comment);
+    await post.save();
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+    console.log("Error in commentOnPost controller: ", error);
+  }
+};
+
+export const likeUnlikePost = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const { id: postId } = req.params;
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+    const userLikedPost = post.likes.includes(userId);
+    if (userLikedPost) {
+      // unlike Post
+      await Post.updateOne({ _id: postId }, { $pull: { likes: userId } });
+      res.status(200).json({ message: "Post unliked successfully" });
+    } else {
+      // like Post
+      post.likes.push(userId);
+      await post.save();
+
+      const notificaton = new Notification({
+        from: userId,
+        to: post.user,
+        type: "like",
+      });
+      await notificaton.save();
+      res.status(200).json({ message: "Post liked successfully" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+    console.log("Error in likeUnlinePost controller: ", error);
   }
 };
